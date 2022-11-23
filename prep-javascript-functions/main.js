5ed395b4aada77f5aea9a7a02deeb1f6ee48b8e9
Jimmy Wu <jameswu49@gmail.com>
2022-11-23 04:58:53 +0000

    prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(2, 2));
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+console.log(convertHoursToMinutes(2));
+
+function getGreeting(name) {
+  return 'Hello ' + name;
+}
+
+console.log(getGreeting('World'));
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+console.log(addAndMultiplyBy5(10, 5));
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+
+console.log(multiplyAndDivideBy5(35, 10));
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(subtractTwoNumbers(22, 7));
+
+function getCircleCurcumfrence(radius) {
+  return 2 * Math.PI * radius;
+}
+
+console.log(getCircleCurcumfrence(5));
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+console.log(getFullName('Jimmy', 'Wu'));
+
+function cube(number) {
+  return Math.pow(number, 3);
+}
+
+console.log(cube(5));
